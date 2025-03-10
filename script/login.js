c27e8ecd9e1eb86f7e2d01222c55655524a7c6bb
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-03-10 11:42:28 +0600

    added all file update

--- script/login.js
+++ script/login.js
@@ -10,7 +10,7 @@ document
     if (accountNumber.length === 11) {
 
       if (convertedPin === 1234) {
-        // console.log("okey tikh ache");
+      
         window.location.href="./main.html"
       } 
       else {
@@ -18,123 +18,6 @@ document
       }
     } 
     else {
-      alert("need valid acount number");
+      alert("need valid account number");
     }
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document
-//   .getElementById("login-btn")
-//   .addEventListener("click", function (event) {
-//     // console.log(event);
-//     event.preventDefault();
-
-//     const accountNumber = document.getElementById("account-number").value;
-//     const pin = document.getElementById("pin").value;
-//     const convertedPin = parseInt(pin);
-//     // console.log(accountNumber.length);
-//     console.log(typeof pin);
-//     if (accountNumber.length === 11) {
-//       if (convertedPin === 1234) {
-//         console.log("okey tikh ache");
-//       } else {
-//         console.log("pin thik nai");
-//       }
-//     } else {
-//       console.log("need valid account number");
-//     }
-//   });
-
-// document.addEventListener('click', function(event ){
-//     console.log("clicked Element:", event.srcElement);
-// });
-
-// document.addEventListener("click", function(event){
-//     event.target.style.backgroundColor = "yellow";
-// });
-
-// document.addEventListener("click", function(event){
-//     if(event.target.tagName === "BUTTON"){
-//         console.log("A button was clicked!");
-//     }
-// });
-
-// document.getElementById("parent").addEventListener("click", function(event){
-//     // console.log("Target:", event.target);
-//     console.log("Current Target:", event.currentTarget);
-// });
-
-// document.addEventListener("click", function(event){
-//     console.log(event.view);
-// });
-
-// document.addEventListener("keydown", function(event) {
-//     console.log("Event View:", event.view); // Output: Window {...}
-// });
-
-// document.addEventListener("click", function(event) {
-//     let win = event.view;
-//     console.log("Window Width:", win.innerWidth);
-// });
-
-// document.addEventListener('click', function(event){
-//     let win = event.view;
-//     console.log("window Width:", win.innerWidth);
-// });
-
-// document.addEventListener("keydown", function(event) {
-//     console.log("Event View:", event.view); // Output: Window {...}
-// });
-
-// // alert ডায়ালগ দেখানো
-// window.alert("Hello, World!");
-
-// fetch API ব্যবহার করে ডেটা আনয়ন
-// window.fetch("https://api.example.com/data")
-// //   .then(response => response.json())
-// //   .then(data => console.log(data));
-
-// // localStorage এ ডেটা স্টোর করা
-// window.localStorage.setItem("name", "John Doe");
-
-// // location ব্যবহার করে নতুন URL এ নেভিগেট করা
-// window.location.href = "https://www.google.com";
---
+++ script/trial.js
@@ -0,0 +1,47 @@
+// let x = 26;
+// let y = 52;
+// console.log("value of x=" + x);
+// console.log("value of y=" + y);
+
+// let str = "Hello ALL";
+// let str1 = "";
+// console.log("Value of str=" + str);
+// console.log("Value of str1=" + str1);
+
+// let str = "ami nai tmi nai";
+// console.log("str=" );
+
+// let x;
+// console.log(x);
+
+// let x;
+// console.log(x);
+
+
+const name = document.getElementsByClassName("name");
+const name = document.getElementById("name");
+console.log(name.innerText)
+
+const allTag = document.getElementsByClassName("name");
+
+
+
+
+document.getElementById("click").addEventListener("click", function () {
+    document.getElementById("country").style.color = "red";
+
+})
+
+
+const name = document.getElementById("name").innerText;
+const price = document.getElementById("price").value;
+
+console.log(price)
+
+const container = document.getElementById("container");
+
+const p = document.createElement("p");
+p.innerText="Hero ALom"
+container.appendChild(p)
+
+
